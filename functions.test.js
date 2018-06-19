c802df6506e9c987397e16b50dd915806c68a719
andyliu-417 <andy2018.liu@gmail.com>
2018-06-19 11:36:29 -0400

    change test to it

--- functions.test.js
+++ functions.test.js
@@ -13,24 +13,24 @@ const nameCheck = () => console.log('Checking Name....');
 describe('Checking Names', () => {
   beforeEach(() => nameCheck());
 
-  test('User is Jeff', () => {
+  it('User is Jeff', () => {
     const user = 'Jeff';
     expect(user).toBe('Jeff');
   });
 
-  test('User is Karen', () => {
+  it('User is Karen', () => {
     const user = 'Karen';
     expect(user).toBe('Karen');
   });
 });
 
 // toBe
-test('Adds 2 + 2 to equal 4', () => {
+it('Adds 2 + 2 to equal 4', () => {
   expect(functions.add(2, 2)).toBe(4);
 });
 
 // not
-test('Adds 2 + 2 to NOT equal 5', () => {
+it('Adds 2 + 2 to NOT equal 5', () => {
   expect(functions.add(2, 2)).not.toBe(5);
 });
 
@@ -42,17 +42,17 @@ test('Adds 2 + 2 to NOT equal 5', () => {
 // toBeFalsy matches anything that an if statement treats as false
 
 // toBeNull
-test('Should be null', () => {
+it('Should be null', () => {
   expect(functions.isNull()).toBeNull();
 });
 
 // toBeFalsy
-test('Should be falsy', () => {
+it('Should be falsy', () => {
   expect(functions.checkValue(undefined)).toBeFalsy();
 });
 
 // toEqual
-test('User should be Brad Traversy object', () => {
+it('User should be Brad Traversy object', () => {
   expect(functions.createUser()).toEqual({
     firstName: 'Brad',
     lastName: 'Traversy'
@@ -60,19 +60,19 @@ test('User should be Brad Traversy object', () => {
 });
 
 // Less than and greater than
-test('Should be under 1600', () => {
+it('Should be under 1600', () => {
   const load1 = 800;
   const load2 = 800;
   expect(load1 + load2).toBeLessThanOrEqual(1600);
 });
 
 // Regex
-test('There is no I in team', () => {
+it('There is no I in team', () => {
   expect('team').not.toMatch(/I/i);
 });
 
 // Arrays
-test('Admin should be in usernames', () => {
+it('Admin should be in usernames', () => {
   usernames = ['john', 'karen', 'admin'];
   expect(usernames).toContain('admin');
 });
@@ -80,7 +80,7 @@ test('Admin should be in usernames', () => {
 // Working with async data
 
 // Promise
-// test('User fetched name should be Leanne Graham', () => {
+// it('User fetched name should be Leanne Graham', () => {
 //   expect.assertions(1);
 //   return functions.fetchUser().then(data => {
 //     expect(data.name).toEqual('Leanne Graham');
@@ -88,7 +88,7 @@ test('Admin should be in usernames', () => {
 // });
 
 // Async Await
-test('User fetched name should be Leanne Graham', async () => {
+it('User fetched name should be Leanne Graham', async () => {
   expect.assertions(1);
   const data = await functions.fetchUser();
   expect(data.name).toEqual('Leanne Graham');
